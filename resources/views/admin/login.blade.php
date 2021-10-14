710e030cb8f4ade1011cee82c039c1f4821af375
dangnambeo <dangnamnguyen99@gmail.com>
2021-10-14 14:21:54 +0700

    c

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,94 +6,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin_asset/login/vendor/bootstrap/css/bootstrap.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin_asset/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin_asset/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin_asset/login/vendor/animate/animate.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin_asset/login/vendor/css-hamburgers/hamburgers.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin_asset/login/vendor/animsition/css/animsition.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin_asset/login/vendor/select2/select2.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin_asset/login/vendor/daterangepicker/daterangepicker.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin_asset/login/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin_asset/login/css/main.css') }}">
     <!--===============================================================================================-->
 </head>
 
 <body>
 
-    <div class="limiter">
-        <div class="container-login100">
-            <div class="wrap-login100">
-                <div class="login100-form-title" style="background-image: url(images/bg-01.jpg);">
-                    <span class="login100-form-title-1">
-                        Sign In
-                    </span>
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-form-title" style="background-image: url(images/blog.jpg);">
+                <span class="login100-form-title-1">
+                    Đăng nhập
+                </span>
+            </div>
+
+            <form class="login100-form validate-form" action="{{ route('post-login') }}" method="POST">
+                <div class="wrap-input100 validate-input m-b-26" data-validate="Không được để trống ô này">
+                    <span class="label-input100">Tên đăng nhập</span>
+                    <input class="input100" type="text" name="user_name" placeholder="">
+                    <span class="focus-input100"></span>
                 </div>
 
-                <form class="login100-form validate-form">
-                    <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-                        <span class="label-input100">Username</span>
-                        <input class="input100" type="text" name="username" placeholder="Enter username">
-                        <span class="focus-input100"></span>
-                    </div>
-
-                    <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
-                        <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="pass" placeholder="Enter password">
-                        <span class="focus-input100"></span>
-                    </div>
-
-                    <div class="flex-sb-m w-full p-b-30">
-                        <div class="contact100-form-checkbox">
-                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-                            <label class="label-checkbox100" for="ckb1">
-                                Remember me
-                            </label>
-                        </div>
-
-                        <div>
-                            <a href="#" class="txt1">
-                                Forgot Password?
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="container-login100-form-btn">
-                        <button class="login100-form-btn">
-                            Login
-                        </button>
-                    </div>
-                </form>
-            </div>
+                <div class="wrap-input100 validate-input m-b-18" data-validate="Không được để trống ô này">
+                    <span class="label-input100">Mật khẩu</span>
+                    <input class="input100" type="password" name="password" placeholder="">
+                    <span class="focus-input100"></span>
+                </div>
+                <div class="container-login100-form-btn">
+                    <button type="submit" name="submit" class="login100-form-btn">
+                        Đăng nhập
+                    </button>
+                </div>
+            </form>
         </div>
+    </div>
     </div>
 
     <!--===============================================================================================-->
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="{{ asset('admin_asset/login/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <script src="{{ asset('admin_asset/login/vendor/animsition/js/animsition.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ asset('admin_asset/login/vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('admin_asset/login/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/select2/select2.min.js"></script>
+    <script src="{{ asset('admin_asset/login/vendor/select2/select2.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/daterangepicker/moment.min.js"></script>
-    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <script src="{{ asset('admin_asset/login/vendor/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('admin_asset/login/vendor/daterangepicker/daterangepicker.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <script src="{{ asset('admin_asset/login/vendor/countdowntime/countdowntime.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="js/main.js"></script>
+    <script src="{{ asset('admin_asset/login/js/main.js') }}"></script>
 
 </body>
 
